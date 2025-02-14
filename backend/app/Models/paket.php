5b7e93b7a5981ca6cc37e9dc9412a1c0c5048fd0
Arafian Hafiz <arafianw@gmail.com>
2025-02-14 19:51:34 +0700

    p

---
+++ backend/app/Models/paket.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class paket extends Model
+{
+    protected $table = 'pakets';
+    protected $fillable = ['nama_paket', 'harga', 'deskripsi'];
+}
