5107616d44432d509506020ad3636b82d9562221
Arafian Hafiz <arafianw@gmail.com>
2025-02-15 23:27:11 +0700

    done

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PaketList/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PaketList from './components/PaketList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PaketList,
   }
 }
 </script>
