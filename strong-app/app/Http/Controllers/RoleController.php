a8c5759e238b4b29f080c89551a1af0f998b8c66
Arafian Hafiz <arafianw@gmail.com>
2025-02-25 23:28:14 +0700

    role index

--- strong-app/app/Http/Controllers/RoleController.php
+++ strong-app/app/Http/Controllers/RoleController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    //
+    public function index()
+    {
+        $role = role::paginate(5);
+        return view('page.role.index')->with([
+            'role' => $role
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $data = [
+            'nama' => $request->input('nama'),
+        ];
+
+        role::create($data);
+
+        return back()->with('message_delete', 'Data Supplier Sudah dihapus');
+    }
+    
+    public function update(Request $request, string $id)
+    {
+        $data = [
+            'nama' => $request->input('nama'),
+        ];
+
+        $datas = role::findOrFail($id);
+        $datas->update($data);
+        return back()->with('message_delete', 'Data Supplier Sudah dihapus');
+    }
+
+    public function destroy(string $id)
+    {
+        $data = role::findOrFail($id);
+        $data->delete();
+        return back()->with('message_delete','Data Supplier Sudah dihapus');
+    }
 }
