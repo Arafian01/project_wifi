68d64ca3dbaf8e43f6bb4bf3a1641d7dd59ac685
Arafian Hafiz <arafianw@gmail.com>
2025-03-16 15:35:40 +0700

    Create form-edit.blade.php

---
+++ strong-app/resources/views/page/pelanggan/form-edit.blade.php
@@ -0,0 +1,15 @@
+<div class="space-y-4">
+    <!-- Tambahkan field edit yang diperlukan -->
+    <!-- Contoh: -->
+    <div>
+        <x-input-label for="edit_nama" value="Nama Lengkap" />
+        <x-text-input 
+            id="edit_nama" 
+            name="name" 
+            type="text"
+            class="mt-1 w-full"
+            required
+        />
+    </div>
+    <!-- ... (field lainnya) ... -->
+</div>
