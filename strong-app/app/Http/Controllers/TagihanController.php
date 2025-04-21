52eb6df5e998e4bcd7f3dcf652a383fa47fc6287
Arafian Hafiz <arafianw@gmail.com>
2025-04-21 10:56:02 +0700

    Update TagihanController.php

--- strong-app/app/Http/Controllers/TagihanController.php
+++ strong-app/app/Http/Controllers/TagihanController.php
@@ -11,48 +11,64 @@ class TagihanController extends Controller
 {
     public function index()
     {
-        $tagihan = tagihan::paginate(5);
-        $user = User::all();
-        $pelanggan = pelanggan::all();
-        return view('admin.page.tagihan.index')->with([
-            'tagihan' => $tagihan,
-            'pelanggan' => $pelanggan,
-            'user' => $user
-        ]);
+        try {
+            $tagihan = tagihan::paginate(5);
+            $user = User::all();
+            $pelanggan = pelanggan::all();
+            return view('admin.page.tagihan.index')->with([
+                'tagihan' => $tagihan,
+                'pelanggan' => $pelanggan,
+                'user' => $user
+            ]);
+        } catch (\Exception $e) {
+            return redirect()->route('error.index')->with('error_message', 'Error: ' . $e->getMessage());
+        }
     }
 
     public function store(Request $request)
     {
-        $data = [
-            'pelanggan_id' => $request->input('pelanggan_id'),
-            'bulan_tahun' => $request->input('bulan_tahun'),
-            'status_pembayaran' => $request->input('status_pembayaran'),
-            'jatuh_tempo' => $request->input('jatuh_tempo'),
-        ];
+        try {
+            $data = [
+                'pelanggan_id' => $request->input('pelanggan_id'),
+                'bulan_tahun' => $request->input('bulan_tahun'),
+                'status_pembayaran' => $request->input('status_pembayaran'),
+                'jatuh_tempo' => $request->input('jatuh_tempo'),
+            ];
 
-        tagihan::create($data);
+            tagihan::create($data);
 
-        return back()->with('message_delete', 'Data Supplier Sudah dihapus');
+            return back()->with('message_delete', 'Data Supplier Sudah dihapus');
+        } catch (\Exception $e) {
+            return redirect()->route('error.index')->with('error_message', 'Error: ' . $e->getMessage());
+        }
     }
 
     public function update(Request $request, string $id)
     {
-        $data = [
-            'pelanggan_id' => $request->input('pelanggan_id'),
-            'bulan_tahun' => $request->input('bulan_tahun'),
-            'status_pembayaran' => $request->input('status_pembayaran'),
-            'jatuh_tempo' => $request->input('jatuh_tempo'),
-        ];
+        try {
+            $data = [
+                'pelanggan_id' => $request->input('pelanggan_id'),
+                'bulan_tahun' => $request->input('bulan_tahun'),
+                'status_pembayaran' => $request->input('status_pembayaran'),
+                'jatuh_tempo' => $request->input('jatuh_tempo'),
+            ];
 
-        $datas = tagihan::findOrFail($id);
-        $datas->update($data);
-        return back()->with('message_delete', 'Data Supplier Sudah dihapus');
+            $datas = tagihan::findOrFail($id);
+            $datas->update($data);
+            return back()->with('message_delete', 'Data Supplier Sudah dihapus');
+        } catch (\Exception $e) {
+            return redirect()->route('error.index')->with('error_message', 'Error: ' . $e->getMessage());
+        }
     }
 
     public function destroy($id)
-    {   
-        $data = tagihan::findOrFail($id);
-        $data->delete();
-        return back()->with('message_delete','Data Supplier Sudah dihapus');
+    {
+        try {
+            $data = tagihan::findOrFail($id);
+            $data->delete();
+            return back()->with('message_delete', 'Data Supplier Sudah dihapus');
+        } catch (\Exception $e) {
+            return redirect()->route('error.index')->with('error_message', 'Error: ' . $e->getMessage());
+        }
     }
 }
