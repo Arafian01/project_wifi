ff4ceee5b3a9385f1d1fee99f6537f89c61f5afa
Arafian Hafiz <arafianw@gmail.com>
2025-05-05 22:43:56 +0700

    Update index.blade.php

--- strong-app/resources/views/admin/page/laporan/index.blade.php
+++ strong-app/resources/views/admin/page/laporan/index.blade.php
@@ -23,7 +23,8 @@
                                 <div class="mb-5 w-full">
                                     <label for="bulan_tahun"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Bulan Tahun</label>
-                                    <input type="year" id="bulan_tahun" name="bulan_tahun"
+                                    <input type="number" id="bulan_tahun" name="bulan_tahun"
+                                    min="2020" max="2099" step="1"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         />
                                 </div>
